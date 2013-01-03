dff952d2a79d7da4e0b88dcb1639cde047fe9726
franck <carnet.franck.paul@gmail.com>
2013-01-03 10:30:25 +0100

    Adjust tag's level css color

--- ductilePhoto/_define.php
+++ ductilePhoto/_define.php
@@ -2,7 +2,7 @@
 # -- BEGIN LICENSE BLOCK ---------------------------------------
 # This file is part of Ductile, a theme for Dotclear
 #
-# Copyright (c) 2011 - Association Dotclear
+# Copyright (c) 2013 - Association Dotclear
 # Licensed under the GPL version 2.0 license.
 # See LICENSE file or
 # http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
@@ -14,7 +14,7 @@ $this->registerModule(
 	/* Name */				"Ductile Photo",
 	/* Description*/		"Variation de Ductile pour un photoblog",
 	/* Author */			"Kozlika et Franck Paul",
-	/* Version */			'0.4'
+	/* Version */			'0.5'
 	/* Properties (uncomment for 2.4 compliance only : add a / at the end of this line -> ) *
 							,array(
 								'standalone_config' => true
